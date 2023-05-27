f9b88a4008e6cbf8feb05560a591bf3646ce9da9
Greg M. Thompson <gthompson333@gmail.com>
2023-05-27 09:46:00 -0700

    "New Event" screen.

--- lib/ui/screens/add_event_screen.dart
+++ lib/ui/screens/add_event_screen.dart
@@ -1,10 +1,78 @@
 import 'package:flutter/material.dart';
+import 'package:medication_events/network/models/medication_event.dart';
 
-class AddEventScreen extends StatelessWidget {
+class AddEventScreen extends StatefulWidget {
   const AddEventScreen({super.key});
 
   @override
+  AddEventScreenState createState() => AddEventScreenState();
+}
+
+class AddEventScreenState extends State<AddEventScreen> {
+  late String _name, _timestamp;
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Add Medication Event"),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            nameTextField(),
+            const SizedBox(
+              height: 10.0,
+            ),
+            timestampTextField(),
+            const SizedBox(
+              height: 10.0,
+            ),
+            submitButton(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  TextField nameTextField() {
+    return TextField(
+      decoration: const InputDecoration(
+        border: OutlineInputBorder(),
+        hintText: "Medication Name",
+        labelText: "Name",
+      ),
+      onChanged: (value) {
+        _name = value;
+      },
+    );
+  }
+
+  TextField timestampTextField() {
+    return TextField(
+      decoration: const InputDecoration(
+        border: OutlineInputBorder(),
+        hintText: "Date and Time",
+        labelText: "Date and Time",
+      ),
+      onChanged: (value) {
+        _timestamp = value;
+      },
+    );
+  }
+
+  ElevatedButton submitButton() {
+    return ElevatedButton(
+      onPressed: () {
+        final event = MedicationEvent(
+            id: 0, datetime: _timestamp, medication: _name, medicationtype: "");
+      },
+      child: const Text(
+        "Add",
+        style: TextStyle(color: Colors.blue),
+      ),
+    );
   }
 }
